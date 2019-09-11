9d1e8ebcb41f6865f42bded08d7c48b74458854f
davids-dev <davidskovgaard@gmail.com>
2019-09-11 15:56:26 +0200

    Autopush form awesome script

---
+++ david/libitmal/dummy.py
@@ -0,0 +1,21 @@
+####### DUMMY MODULE ###########
+
+#!/opt/anaconda3/bin/python
+
+
+import numpy as np
+import contextlib as ctxlib
+import collections
+import sklearn
+import random
+
+from math import inf, nan, fabs
+from numpy import linalg
+
+# NOTE: for VarName
+import inspect
+import re
+
+def hello():
+	print('Hello world')
+	return 

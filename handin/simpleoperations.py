33a8923fa04644d0b476f0820d8e6169b37ee99c
johnromby <romby@outlook.com>
2019-09-12 14:20:36 +0200

    Handin L01 Modules and Classes to review

---
+++ handin/simpleoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Welcome Hiro's Calculator
+# 
+# This calculator will contain simple function for mathematical operations
+
+# In[4]:
+
+
+def Add(x,y):
+    myvar = x+y
+    print("Addition called V1.0")
+    return myvar
+
+def Sub(x,y):
+    myvar= x-y
+    print("Subtraction called V1.0")
+    return myvar 
+
+def Mul(x,y):
+    myvar = x*y
+    print("Multiplication called V1.0")
+    return myvar 
+
+def Div(x,y):
+    myvar = x/y
+    print("Multiplication called V1.0")
+    return myvar
+
